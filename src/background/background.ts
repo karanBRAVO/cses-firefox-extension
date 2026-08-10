e107ce6037cb69f926caf809fb2e3ae10ffac8b3
D@reK@r@n18 <xpresskaran98@gmail.com>
2026-08-11 02:41:33 +0530

    send message from content to background

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,1 +1,11 @@
+declare const browser: any;
+
 console.log("CSES Companion background loaded!");
+
+browser.runtime.onMessage.addListener((message: any) => {
+  console.log("Message received from content script:", message);
+
+  if (message.type === "PROBLEM_DETECTED") {
+    console.log("Detected CSES problem:", message.problem);
+  }
+});
--- src/content/content.ts
+++ src/content/content.ts
@@ -1,3 +1,7 @@
+import type { CSESProblem } from "../types/problem";
+
+declare const browser: any;
+
 console.log("CSES Companion loaded!");
 console.log("Current URL:", window.location.href);
 
@@ -18,3 +22,18 @@ const problemTitle = getProblemTitle();
 
 console.log("Problem ID:", problemId);
 console.log("Problem Title:", problemTitle);
+
+if (problemId && problemTitle) {
+  const problem: CSESProblem = {
+    id: problemId,
+    title: problemTitle,
+    url: window.location.href,
+  };
+
+  console.log("CSES Problem object:", problem);
+
+  browser.runtime.sendMessage({
+    type: "PROBLEM_DETECTED",
+    problem,
+  });
+}
